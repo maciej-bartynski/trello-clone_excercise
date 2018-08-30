008c2571ff105ba77b4ce7dc13cd859e0bbd8b43
JestemBartek <maciek.bartynski@gmail.com>
2018-08-31 00:44:03 +0200

    initial text upgrade

--- src/database/initialdata.js
+++ src/database/initialdata.js
@@ -3,19 +3,19 @@ var initialData = [
         column: 'To do',
         cards: [
             {
-                title: 'title of the first one',
+                title: 'Learn latin',
                 description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed facilisis lobortis odio id interdum. Morbi nec lacinia odio. Suspendisse placerat maximus mauris vitae convallis. Aliquam fringilla sapien mauris, id sollicitudin velit rutrum faucibus. Praesent ornare velit quam, ullamcorper pharetra tellus consequat in. Nam feugiat ante sit amet elit feugiat volutpat. Fusce imperdiet enim magna, eu dignissim nisi pulvinar sagittis. ',
                 date: '2018-09-01',
                 color: 'rgba(255, 255, 0, 0.7)'
             },
             {
-                title: 'title of the second one',
-                description: 'Hello from Russia',
+                title: 'Keeping calm and git push',
+                description: 'Hello from Poland',
                 date: '2019-08-21',
                 color: 'rgba(0,255,0,0.4)'
             },
             {
-                title: 'title of the third one',
+                title: 'Title of the third one',
                 description: 'Adipiscing elit. Sed facilisis lobortis odio id interdum. Morbi nec lacinia odio. Suspendisse placerat maximus mauris vitae convallis. Aliquam fringilla sapien mauris, id sollicitudin velit rutrum faucibus. Praesent ornare velit quam, ullamcorper pharetra tellus consequat in. Nam feugiat ante sit amet elit feugiat volutpat.',
                 date: '2019-08-21',
                 color: 'rgba(0,255,0,0.4)'
@@ -23,26 +23,26 @@ var initialData = [
         ]
     },
     {
-        column: 'In progress',
+        column: 'Processing...',
         cards: [{
-                title: 'title of the first one',
+                title: 'Nothing',
                 description: 'Consectetur adipiscing elit. Sed facilisis lobortis odio id interdum. Morbi nec lacinia odio. Suspendisse placerat maximus mauris vitae convallis. Aliquam fringilla sapien mauris, id sollicitudin velit rutrum faucibus. Praesent ornare velit quam, ullamcorper pharetra tellus consequat in. Nam feugiat ante sit amet elit feugiat volutpat. Fusce imperdiet enim magna, eu dignissim nisi pulvinar sagittis.',
                 date: '2019-08-21',
                 color: 'rgba(0,255,0,0.4)'
             },
             {
-                title: 'title of the second one',
-                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed facilisis lobortis odio id interdum. Morbi nec lacinia odio. Suspendisse placerat maximus mauris vitae convallis. Aliquam fringilla sapien mauris, id sollicitudin velit rutrum faucibus. Praesent ornare velit quam, ullamcorper pharetra tellus consequat in. Nam feugiat ante sit amet elit feugiat volutpat. Fusce imperdiet enim magna, eu dignissim nisi pulvinar sagittis. ',
+                title: 'To understand Tolkien better',
+                description: '\"I can\'t recall the taste of food, nor the sound of water.I\'m naked in the dark. There\'s nothing… no veil between me and the wheel of fire.\"',
                 date: '2019-08-21',
                 color: 'rgba(0,255,0,0.4)'
             }
         ]
     },
     {
-        column: 'done',
+        column: 'Done',
         cards: [{
-                title: 'title of the first one',
-                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed facilisis lobortis odio id interdum. Morbi nec lacinia odio. Suspendisse placerat maximus mauris vitae convallis. Aliquam fringilla sapien mauris, id sollicitudin velit rutrum faucibus. Praesent ornare velit quam, ullamcorper pharetra tellus consequat in. Nam feugiat ante sit amet elit feugiat volutpat. Fusce imperdiet enim magna, eu dignissim nisi pulvinar sagittis.',
+                title: 'Lorem ipsum muspi merol',
+                description: 'How much wood would a woodchuck chuck if a woodchuck could chuck wood?',
                 date: '2018-08-21',
                 color: 'rgba(255,0,0,0.4)'
             }
