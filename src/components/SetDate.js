0e7e6ec2dd6b039ecce48f1c524c6b3a624cb652
JestemBartek <maciek.bartynski@gmail.com>
2018-08-31 12:19:31 +0200

    lifecycle methods

--- src/components/SetDate.js
+++ src/components/SetDate.js
@@ -2,17 +2,53 @@ import React, {
     Component
 } from 'react';
 
-const dateinputstyle = {
-    borderRadius: '3px',
-    border: 'solid 1px blue',
-    outline: 'none',
-    marginRight: '10px',
+const dateoutputstyle = {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: 'blue',
+    fontWeight: 'bolder',
+    letterSpacing: '0.1rem',
+    padding: '5px 1%'
+}
+const managingpanel = {
+    width: '100%',
+    backgroundColor: 'rgba(0,0,255,0.2)',
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    fontWeight: 'bolder',
+    letterSpacing: '0.1rem',
+    padding: '10px 5%'
+}
+const dateinput = {
+    backgroundColor: 'rgba(0,0,255,0.4)',
+    border: 'solid 2px white',
+    outline: '0',
+    height: '35px',
+    outline: '0',
+    boxSizing: 'border-box',
+    borderRadius: '5px 0 0 5px',
+    borderLeft: 'none'
+}
+const btninput = {
+    backgroundColor: 'rgba(0,0,255,0.7)',
+    color: 'white',
+    border: 'solid 2px white',
+    borderLeft: 'none',
+    borderRadius: '0 5px 5px 0',
+    outline: '0',
+    height: '35px',
+    width: '50px',
+    padding: '5px',
+    boxSizing: 'border-box',
 }
 class SetDate extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            address: ' ' + this.props.objectid + this.props.cardid + 'date',
+            address: 'setdate' + this.props.objectid + this.props.cardid + 'date',
             stylethis: {
                 width: '100%',
                 display: 'flex',
@@ -21,67 +57,68 @@ class SetDate extends Component {
                 color: 'blue',
                 padding: '1px 0 1px 5px',
                 flexWrap: 'wrap',
+            },
+            colorthis: {
                 backgroundColor: this.props.markup
             }
         }
         this.deadline = this.deadline.bind(this);
+        this.setthecolor = this.setthecolor.bind(this)
+    }
+    setthecolor(date){ 
+        console.log('setcolor1')
+       let year = parseFloat(date.innerText.charAt(0) + date.innerText.charAt(1) + date.innerText.charAt(2) + date.innerText.charAt(3));
+       let month = parseFloat(date.innerText.charAt(5) + date.innerText.charAt(6));
+       let day = parseFloat(date.innerText.charAt(8) + date.innerText.charAt(9));
+       let currentDate = new Date();
+       let currDay = parseFloat(currentDate.getDate());
+       let currMonth = parseFloat(currentDate.getMonth() + 1);
+       let currYear = parseFloat(currentDate.getFullYear());
+       let currTotal = ((currYear - 2000) * 12) + currMonth;
+       let setTotal = ((year - 2000) * 12) + month;
+       if (currTotal>setTotal){
+           return 'rgba(255,0,0,0.4)'
+       } else if (currTotal<setTotal) {
+            return 'rgba(0,255,0,0.4)'
+       } else {
+           if (currDay<day-3){
+              return 'rgba(0,255,0,0.4)'
+           } else if (currDay>day-3 && currDay<=day) {
+                return 'rgba(255,255,0,0.4)'
+           } else {
+               return 'rgba(255,0,0,0.4)'
+           }
+       }
     }
     deadline() {
-        let date = document.getElementById(this.state.address);
-        document.getElementById(this.state.address + 'outp').innerText = date.value;
-        document.getElementById(this.state.address + 'info').innerText = ' Deadline: ';
-        let currentData = JSON.parse(localStorage.getItem('initialData'));
-        currentData[this.props.objectid]['cards'][this.props.cardid]['date'] = date.value;
-        //color part
-        var setColor;
-        let year = parseFloat(date.value.charAt(0) + date.value.charAt(1) + date.value.charAt(2) + date.value.charAt(3));
-        let month = parseFloat(date.value.charAt(5) + date.value.charAt(6));
-        let day = parseFloat(date.value.charAt(8) + date.value.charAt(9));
-        let currentDate = new Date();
-        let currDay = parseFloat(currentDate.getDate());
-        let currMonth = parseFloat(currentDate.getMonth()+1);
-        let currYear = parseFloat(currentDate.getFullYear());
-        
-        let currTotal = ((currYear-2000)*12)+currMonth;
-        let setTotal = ((year - 2000) * 12) + month;
-        if (currTotal>setTotal){
-            setColor = 'rgba(255,0,0,0.4';
-        } else if (currTotal === setTotal) {
-            if (currDay > day) {
-                setColor = 'rgba(255,0,0,0.4)';
-            } else if ((currDay > (day - 3)) && (currDay <= day)) {
-                setColor = 'rgba(255,255,0,0.7)';
-            } else {
-                setColor = 'rgba(0,255,0,0.4)';
-            }
-        } else {
-            setColor = 'rgba(0,255,0,0.4)';
+        let date = document.getElementById(this.state.address+'dateinp');
+        if (date.value){
+            let currentData = JSON.parse(localStorage.getItem('initialData'));
+            currentData[this.props.objectid]['cards'][this.props.cardid]['date'] = date.value;   
+            localStorage.setItem('initialData', JSON.stringify(currentData));
+            this.props.newstorage();
         }
-        //
-        currentData[this.props.objectid]['cards'][this.props.cardid]['color'] = setColor;
-        localStorage.setItem('initialData', JSON.stringify(currentData));
-        this.props.newstorage();
     }
     componentDidMount() {
-        let date = document.getElementById(this.state.address);
-        let its = this;
-        date.addEventListener('change', function(){
-            its.deadline();
-        })
+        if(this.props.date){
+            let dateDispaly = document.querySelector('#' + this.state.address + 'outp');
+            dateDispaly.style.backgroundColor = this.setthecolor(dateDispaly)
+        }
+    }
+    componentDidUpdate() {
+        if (this.props.date) {
+            let dateDispaly = document.querySelector('#' + this.state.address + 'outp');
+            dateDispaly.style.backgroundColor = this.setthecolor(dateDispaly)
+        }
     }
     render() {
         return (
             this.props.date?
-                <div style={this.state.stylethis}>
-                    <input style={dateinputstyle} id={this.state.address} type='date'/>
-                    <div id={this.state.address+'info'}>Deadline:</div>
-                    <div id={this.state.address+'outp'}>{this.props.date}</div>
-                </div>
+                <div style= {dateoutputstyle} id={this.state.address+'outp'}>{this.props.date}</div>
             :
-                <div style={this.state.stylethis}>
-                    <input style={dateinputstyle} id={this.state.address} type='date'/>
-                    <div id={this.state.address+'info'}></div>
-                    <div id={this.state.address+'outp'}></div>
+                <div style= {managingpanel}>
+                    <input style={dateinput} id={this.state.address+'dateinp'} type='date'/>
+                    <button onClick={this.deadline} style={btninput} id={this.state.address+'datebtn'}>Ok</button>
                 </div>
            );
     }
--- src/database/initialdata.js
+++ src/database/initialdata.js
@@ -6,19 +6,16 @@ var initialData = [
                 title: 'Learn latin',
                 description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed facilisis lobortis odio id interdum. Morbi nec lacinia odio. Suspendisse placerat maximus mauris vitae convallis. Aliquam fringilla sapien mauris, id sollicitudin velit rutrum faucibus. Praesent ornare velit quam, ullamcorper pharetra tellus consequat in. Nam feugiat ante sit amet elit feugiat volutpat. Fusce imperdiet enim magna, eu dignissim nisi pulvinar sagittis. ',
                 date: '2018-09-01',
-                color: 'rgba(255, 255, 0, 0.7)'
             },
             {
                 title: 'Keeping calm and git push',
                 description: 'Hello from Poland',
-                date: '2019-08-21',
-                color: 'rgba(0,255,0,0.4)'
+                date: '2019-01-01',
             },
             {
                 title: 'Title of the third one',
                 description: 'Adipiscing elit. Sed facilisis lobortis odio id interdum. Morbi nec lacinia odio. Suspendisse placerat maximus mauris vitae convallis. Aliquam fringilla sapien mauris, id sollicitudin velit rutrum faucibus. Praesent ornare velit quam, ullamcorper pharetra tellus consequat in. Nam feugiat ante sit amet elit feugiat volutpat.',
-                date: '2019-08-21',
-                color: 'rgba(0,255,0,0.4)'
+                date: '2018-08-31',
             }
         ]
     },
@@ -27,14 +24,12 @@ var initialData = [
         cards: [{
                 title: 'Nothing',
                 description: 'Consectetur adipiscing elit. Sed facilisis lobortis odio id interdum. Morbi nec lacinia odio. Suspendisse placerat maximus mauris vitae convallis. Aliquam fringilla sapien mauris, id sollicitudin velit rutrum faucibus. Praesent ornare velit quam, ullamcorper pharetra tellus consequat in. Nam feugiat ante sit amet elit feugiat volutpat. Fusce imperdiet enim magna, eu dignissim nisi pulvinar sagittis.',
-                date: '2019-08-21',
-                color: 'rgba(0,255,0,0.4)'
+                date: '2018-08-30',
             },
             {
                 title: 'To understand Tolkien better',
                 description: '\"I can\'t recall the taste of food, nor the sound of water.I\'m naked in the dark. There\'s nothing… no veil between me and the wheel of fire.\"',
-                date: '2019-08-21',
-                color: 'rgba(0,255,0,0.4)'
+                date: '2016-08-21',
             }
         ]
     },
@@ -43,8 +38,7 @@ var initialData = [
         cards: [{
                 title: 'Lorem ipsum muspi merol',
                 description: 'How much wood would a woodchuck chuck if a woodchuck could chuck wood?',
-                date: '2018-08-21',
-                color: 'rgba(255,0,0,0.4)'
+                date: '2018-08-31',
             }
         ]
     }
